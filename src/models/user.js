fe870b086dc9d1f1cc360dc54829b07944d51cc0
Abanoub Fathy <abanoub.fathy.me@gmail.com>
2022-02-04 13:16:14 +0200

    add user end points

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const { default: isEmail } = require("validator/lib/isemail");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 // create a user schema
 const userSchema = new mongoose.Schema({
@@ -41,6 +43,60 @@ const userSchema = new mongoose.Schema({
     },
   ],
 });
+
+// hash user passwords before saving to the DB
+userSchema.pre("save", async function (next) {
+  const user = this;
+
+  // if the user try to modify the password
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+
+  next();
+});
+
+// generate user auth token function
+userSchema.methods.generateAuthToken = async function () {
+  const user = this;
+
+  // create new token and save it to the DB
+  const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET, {
+    expiresIn: "1 day",
+  });
+  user.tokens.push({ token });
+  await user.save();
+
+  // return the token
+  return token;
+};
+
+// hide user sensitive information before sending
+userSchema.methods.toJSON = function () {
+  const user = this;
+  const userObj = user.toObject();
+  delete userObj.password;
+  delete userObj.tokens;
+  return userObj;
+};
+
+// find user by credentials function
+userSchema.statics.findByCredentials = async function (email, password) {
+  // find the user by email first
+  const user = await User.findOne({ email });
+  if (!user) {
+    return null;
+  }
+
+  // check for user password
+  const isCorrectPassword = await bcrypt.compare(password, user.password);
+  if (!isCorrectPassword) {
+    return null;
+  }
+
+  // return the user
+  return user;
+};
 
 // create user model
 const User = mongoose.model("User", userSchema);
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,16 +1,155 @@
 const express = require("express");
 const User = require("../models/user");
+const { isValidObjectId } = require("mongoose");
 
 // create a user router
 const router = new express.Router();
 
+// create new normal user
 router.post("/new", async (req, res) => {
-  const user = new User(req.body);
   try {
+    const user = new User(req.body);
+    user.role = "normal";
     await user.save();
+
     res.status(201).send(user);
   } catch (e) {
     res.status(400).send({ error: e.message });
+  }
+});
+
+// create new admin user
+router.post("/new/admin", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    user.role = "admin";
+    await user.save();
+
+    res.status(201).send(user);
+  } catch (e) {
+    res.status(400).send({ error: e.message });
+  }
+});
+
+// create new user-manager
+router.post("/new/manager", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    user.role = "manager";
+    await user.save();
+
+    res.status(201).send(user);
+  } catch (e) {
+    res.status(400).send({ error: e.message });
+  }
+});
+
+// login user
+router.post("/login", async (req, res) => {
+  // destructure the email and password from the body
+  const { email, password } = req.body;
+  try {
+    if (!email || !email.trim() || !password) {
+      return res
+        .status(400)
+        .send({ error: "E-mail and password are required!" });
+    }
+
+    // find the user by email and password
+    const user = await User.findByCredentials(email, password);
+
+    if (!user) {
+      return res.status(400).send({ error: "Not correct Email or Password" });
+    }
+
+    // generate token and return user data
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
+  } catch (e) {
+    res.status(500).send({ error: e.message });
+  }
+});
+
+// read all users in the database
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.send(users);
+  } catch (e) {
+    res.status(500).send({ error: e.message });
+  }
+});
+
+// read a user by its id
+router.get("/:id", async (req, res) => {
+  // check if the id is valid
+  if (!isValidObjectId(req.params.id)) {
+    return res.status(400).send({ error: "Not valid objectId" });
+  }
+
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).send({ error: "User not found!" });
+    }
+
+    res.send(user);
+  } catch (e) {
+    res.status(500).send({ error: e.message });
+  }
+});
+
+// update user by id
+router.patch("/:id", async (req, res) => {
+  const validUpdates = ["name", "email", "password"];
+  const updates = Object.keys(req.body);
+  // check if the updates are valid
+  const isValidUpdate = updates.every((update) =>
+    validUpdates.includes(update)
+  );
+  if (!isValidUpdate) {
+    return res.status(400).send({ error: "Not valid update operation" });
+  }
+
+  // check if the user id is valid
+  if (!isValidObjectId(req.params.id)) {
+    return res.status(400).send({ error: "Not valid id" });
+  }
+  try {
+    // fetch the user we need to update
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).send({ error: "User not found" });
+    }
+
+    // update the fields we need to update
+    updates.forEach((update) => {
+      user[update] = req.body[update];
+    });
+    await user.save();
+    res.send(user);
+  } catch (e) {
+    res.status(400).send({ error: e.message });
+  }
+});
+
+// delete user by id
+router.delete("/:id", async (req, res) => {
+  if (!isValidObjectId(req.params.id)) {
+    return res.status(400).send({ error: "Not valid id" });
+  }
+
+  try {
+    // fetch the user by its id
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).send({ error: "User Not Found" });
+    }
+
+    await user.remove();
+    res.send(user);
+  } catch (e) {
+    res.status(500).send({ error: e.message });
   }
 });
 
